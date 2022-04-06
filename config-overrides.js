c011e91d8bf4ee9c92418941e14cb842bcc5d625
mkngroup <asparpazarlamamehmet@gmail.com>
2022-04-06 16:55:16 +0300

    04.06.2022 10th

--- config-overrides.js
+++ config-overrides.js
@@ -1,10 +1,5 @@
 module.exports = {
-  entry: "./src/app/index.js",
-  output: {
-      path: path.resolve(__dirname, 'build'),
-      publicPath: 'build',
-      filename: 'bundle-main.js'
-  },
+  
   devServer: {
       historyApiFallback:{
           index:'build/index.html'
